335053733f9bc5ad6626a28479efafeec31077b6
Brandon R. Stoner <bstoner@yrsi.com>
2011-07-28 23:02:47 +0000

    Modified NodeJS to be distributed through deb files.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,68 +1,28 @@
 class nodejs {
-	# NodeJS evolves too fast. We need to keep it up to date by compiling.
-	# So, we will also need some compilers.
-	package {
-		"build-essential":
-			ensure => latest,
-	}
-
-	package {
-		"libssl-dev":
-			ensure => latest,
-	}
-
-	$nodejs_location = "http://nodejs.org/dist/v0.5.2/"
-	$nodejs_archive = "node-v0.5.2.tar.gz"
-	$nodejs_prefix = "/usr/local"
-	$nodejs_build_dir = "/root/.build"
-	$nodejs_source_dir = "${nodejs_build_dir}/node-v0.5.2"
+	$package_containment_dir = "/opt/packages"
+	$package_filename = "nodejs_0.5.2_${architecture}.deb"
+	$package_destination = "${package_containment_dir}/${package_filename}"
+	$package_source = "puppet:///nodejs/${package_filename}"
 
 	file {
-		"/root/.build":
+		$package_containment_dir:
 			ensure => directory,
-			require => [Package["build-essential"],
-			            Package["libssl-dev"]]
+
 	}
 
-	exec {
-		"nodejs-download":
-			command => "wget '${nodejs_location}/${nodejs_archive}'",
-			cwd => "${nodejs_build_dir}",
-			creates => "${nodejs_build_dir}/${nodejs_archive}",
-			path => ["/bin", "/usr/sbin",
-			         "/usr/bin", "/usr/local/bin"],
-			require => File["/root/.build"],
+	file {
+		"${package_destination}":
+			source => "${package_source}",
+			mode => 660,
+			require => File["${package_containment_dir}"],
 	}
 
-	exec {
-		"nodejs-extract":
-			command => "tar -xf ${nodejs_archive}",
-			cwd => "${nodejs_build_dir}",
-			creates => "${nodejs_source_dir}",
-			path => ["/bin", "/usr/sbin",
-			         "/usr/bin", "/usr/local/bin"],
-			require => Exec["nodejs-download"],
-	}
-
-	exec {
-		"nodejs-configure":
-			command => "configure --prefix=${nodejs_prefix}",
-			cwd => "${nodejs_source_dir}",
-			creates => "${nodejs_source_dir}/build",
-			path => ["${nodejs_source_dir}", "/bin", "/usr/sbin",
-			         "/usr/bin", "/usr/local/bin"],
-			require => Exec["nodejs-extract"],
-	}
-
-	exec {
+	package {
 		"nodejs":
-			command => "make install",
-			cwd => "${nodejs_source_dir}",
-			creates => "${nodejs_source_dir}/build/default/node",
-			path => ["${nodejs_source_dir}", "/bin", "/usr/sbin",
-			         "/usr/bin", "/usr/local/bin"],
-			timeout => 0, # This takes a while. No way around that.
-			require => Exec["nodejs-extract"],
+			provider => "dpkg",
+			ensure => installed,
+			source => "${package_destination}",
+			require => File[$package_destination],
 	}
 }
 
